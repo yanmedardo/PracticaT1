87437bc2514ae8bdecc142f2c6d2415961bdb3a3
Yan Medardo Minchan <yanmi@DESKTOP-B7A077C>
2021-09-03 21:09:54 -0500

    commit Uno

---
+++ PracticaT1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PracticaT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
